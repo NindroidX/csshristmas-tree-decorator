333d04e6229bbde2ded84122d307ddce6bfc20bb
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2017-01-20 20:39:14 +0100

    Remove some boilerplate code / styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
 'use strict';
 
 require('../scss/main.scss');
-
-let appContainer = document.querySelector('#app');
-
-appContainer.innerHTML = '<div class="main-component"><h1 class="main-component__title">Welcome!</h1></div>';
